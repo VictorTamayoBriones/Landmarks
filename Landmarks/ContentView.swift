698e4aa405a572a2a7cfd9abe81be26dfed95045
VictorTamayoBriones <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-04-09 20:07:59 -0500

    section two

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -2,8 +2,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, SwiftUI")
-            .padding()
+        Text("Turtle Rock")
+            .font(.title)
+            .foregroundColor(.black)
     }
 }
 
