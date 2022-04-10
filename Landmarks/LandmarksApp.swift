2107861718470c174e07dc78b6fc5bb0103918b0
VictorTamayoBriones <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-04-09 19:48:24 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Victor Tamayo on 09/04/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
